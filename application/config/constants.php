939ca06832415b09c29147e3ed801b4f016520bc
FilipeA <filipera1995@hotmail.com>
2019-08-01 16:45:30 +0100

    Added: New constants
    Added new constants for booking
    Added new constants for authentication

--- application/config/constants.php
+++ application/config/constants.php
@@ -95,6 +95,12 @@ defined('LOGIN_ERROR_PASSWORD')             OR define('LOGIN_ERROR_PASSWORD', 2)
 //AUTHENTICATION (used for control over the authentication result)
 defined('AUTHENTICATION_SUCCESS')           OR define('AUTHENTICATION_SUCCESS', 1); // current user is valid
 defined('AUTHENTICATION_ERROR')             OR define('AUTHENTICATION_ERROR', 0); // current user is invalid
+defined('AUTH_GROUP_ADMIN')                 OR define('AUTH_GROUP_ADMIN', 1); // current user is from the admin group
+defined('AUTH_GROUP_WORKER')                OR define('AUTH_GROUP_WORKER', 1); // current user is from the normal func group
+defined('AUTH_PERMISSIONS_ALL')             OR define('AUTH_PERMISSIONS_ALL', 1); // authentication permission (override)
+defined('AUTH_PERMISSIONS_VIEW')            OR define('AUTH_PERMISSIONS_VIEW', 2); // authentication permission general view
+defined('AUTH_PERMISSIONS_ADD')             OR define('AUTH_PERMISSIONS_ADD', 3); // authentication permission general add
+defined('AUTH_PERMISSIONS_EDIT')            OR define('AUTH_PERMISSIONS_EDIT', 4); // authentication permission general edit
 
 //TOKEN
 defined('TOKEN_VALID')                      OR define('TOKEN_VALID', 1); // current token is valid
@@ -102,4 +108,27 @@ defined('TOKEN_INVALID')                    OR define('TOKEN_INVALID', 0); // cu
 
 //LOAD CONSTANTS (used for default loads of the database)
 defined('LOAD_LAST_NOTIFICATIONS')          OR define('LOAD_LAST_NOTIFICATIONS', 5); //loads last 5notifications
+
+//BOOKING TYPES AND STATES
+defined('BOOKING_STATE_SCHEDULED')                      OR define('BOOKING_STATE_SCHEDULED', 1);
+defined('BOOKING_STATE_BOOKED')                         OR define('BOOKING_STATE_BOOKED', 2);
+defined('BOOKING_STATE_WAITING_CONFIRMATION')           OR define('BOOKING_STATE_WAITING_CONFIRMATION', 3);
+defined('BOOKING_STATE_IN_LAB')                         OR define('BOOKING_STATE_IN_LAB', 4);
+defined('BOOKING_STATE_WAITING_PRICE')                  OR define('BOOKING_STATE_WAITING_PRICE', 5);
+defined('BOOKING_STATE_WAITING_PRICE_CONFIRMATION')     OR define('BOOKING_STATE_WAITING_PRICE_CONFIRMATION', 6);
+defined('BOOKING_STATE_DENIED')                         OR define('BOOKING_STATE_DENIED', 7);
+defined('BOOKING_STATE_WAITING_STOCK')                  OR define('BOOKING_STATE_WAITING_STOCK', 8);
+defined('BOOKING_STATE_IN_REPAIR')                      OR define('BOOKING_STATE_IN_REPAIR', 9);
+defined('BOOKING_STATE_REPAIRED')                       OR define('BOOKING_STATE_REPAIRED', 10);
+defined('BOOKING_STATE_NO_REPAIR')                      OR define('BOOKING_STATE_NO_REPAIR', 11);
+defined('BOOKING_STATE_REPAIRED_INFORMED')              OR define('BOOKING_STATE_REPAIRED_INFORMED', 12);
+defined('BOOKING_STATE_NO_REPAIR_INFORMED')             OR define('BOOKING_STATE_NO_REPAIR_INFORMED', 13);
+defined('BOOKING_STATE_WAITING_WITHDRAW')               OR define('BOOKING_STATE_WAITING_WITHDRAW', 14);
+defined('BOOKING_STATE_BOOKED_DELIVERED_S')             OR define('BOOKING_STATE_BOOKED_DELIVERED_S', 15);
+defined('BOOKING_STATE_BOOKED_DELIVERED_F')             OR define('BOOKING_STATE_BOOKED_DELIVERED_F', 16);
+defined('BOOKING_STATE_BOOKED_CANCELED')                OR define('BOOKING_STATE_BOOKED_CANCELED', 17);
+
+defined('BOOKING_TYPE_BUDGET')                          OR define('BOOKING_TYPE_BUDGET', 1);
+defined('BOOKING_TYPE_GIVE_BUDGET')                     OR define('BOOKING_TYPE_GIVE_BUDGET', 2);
+defined('BOOKING_TYPE_WARRANTY')                        OR define('BOOKING_TYPE_WARRANTY', 3);
 
